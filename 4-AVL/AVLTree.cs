465820e611242c44a8aef4bb107a278ee097397f
QazCetelic <qaz.cetelic@protonmail.com>
2024-09-04 21:22:18 +0200

    Initial commit

---
+++ 4-AVL/AVLTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ALGA
+{
+    public class AVLTree
+    {
+        public Node root;
+
+        public void insert(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isBalanced()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void prettyprint()
+        {
+            if (root != null)
+            {
+                root.prettyprint("→", " ");
+            }
+        }
+    }
+}
---
+++ 4-AVL/Node.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ALGA
+{
+    public class Node
+    {
+        public int number;
+
+        public Node left, right;
+
+        public Node(int number)
+        {
+            this.number = number;
+        }
+
+        public Node rotateLeft()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node rotateRight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void prettyprint(String firstPrefix, String prefix)
+        {
+            Console.WriteLine(firstPrefix + number);
+
+            if (right == null)
+            {
+                Console.WriteLine(prefix + "├── .");
+            }
+            else
+            {
+                right.prettyprint(prefix + "├── ", prefix + "|   ");
+            }
+
+            if (left == null)
+            {
+                Console.WriteLine(prefix + "└── .");
+            }
+            else
+            {
+                left.prettyprint(prefix + "└── ", prefix + "    ");
+            }
+        }
+    }
+}
